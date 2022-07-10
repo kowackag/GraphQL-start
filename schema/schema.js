8a4b3063f51d619a62fc3334da81b0a1dba7a2e3
Malgorzata Kowacka <kowackag@gmail.com>
2022-07-10 17:10:01 +0200

    Add json-server

--- schema/schema.js
+++ schema/schema.js
@@ -1,20 +1,24 @@
 const graphql = require("graphql");
 const _ = require("lodash");
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const axios = require("axios");
 
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const CompanyType = new GraphQLObjectType({
+  name: "Company",
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+  },
+});
 const UserType = new GraphQLObjectType({
   name: "User",
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
+    company: { type: CompanyType },
   },
 });
-
-const users = [
-  { id: "23", firstName: "Bill", age: 20 },
-  { id: "26", firstName: "Laura", age: 25 },
-];
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
@@ -23,7 +27,9 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-        return _.find(users, { id: args.id });
+        return axios
+          .get(`http://localhost:3000/user/${args.id}`)
+          .then((resp) => resp.data);
       },
     },
   },
--- server.js
+++ server.js
@@ -11,5 +11,4 @@ app.use("/graphql", graphqlHTTP({
 
 app.listen(4000, () => {
   console.log("Listening");
-  console.log(schema)
 });
